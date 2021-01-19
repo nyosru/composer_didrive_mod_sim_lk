c21304328bf19ea987f223ced286e6a6e0afe5a1
nyosru <nyos@rambler.ru>
2021-01-19 22:37:37 +0500

    0.0.1

---
+++ src/class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+  класс модуля
+ */
+
+namespace Nyos\mod;
+
+//if (!defined('IN_NYOS_PROJECT'))
+//    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
+
+class SIM_LK {
+
+    public static $ar_autobonus_jm_sp_date = [];
+
+    public static function ii() {
+
+        return $return;
+    }
+
+}
